8f565a9e8069ce751586d05f950b5dfb551d31ff
khaled0915 <khaledsaifullah50956@gmail.com>
2023-12-02 21:03:56 +0600

    created a simple error page

--- src/Page/ErrorPage/ErrorPage.jsx
+++ src/Page/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
         <div>
-            
+            <div className="flex flex-col bg-teal-500 items-center justify-center h-screen">
+      <img src="https://i.ibb.co/qNvgzNx/5203299.jpg" alt="Error" className="w-64 h-auto mb-8" /> {/* Adjust the width as needed */}
+      <div className="text-center">
+        <h2 className="text-2xl text-red-500 font-bold">Oops! Something went wrong</h2>
+        <p className="text-white text-xl font-bold ">We're sorry, but it seems there was an error. Please try again later.</p>
+
+        <Link to='/'> <button className="btn btn-danger bg-orange-500 mt-10"> Go Back  </button> </Link>
+      </div>
+    </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
 
-    <RouterProvider router={router} />
+   <div className='max-w-screen-xl mx-auto'>
+   <RouterProvider router={router} />
+   </div>
+
   </React.StrictMode>,
 )
