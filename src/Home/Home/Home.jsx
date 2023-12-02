39c6897bce8098f8780aaa51cd44f312899ce11a
khaled0915 <khaledsaifullah50956@gmail.com>
2023-12-02 20:34:07 +0600

    created the basic client side and install firebase ,and also created the .env file

---
+++ src/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3> This is Home </h3>
+            <h2> Welcome to EchoMinds </h2>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import ErrorPage from "../Page/ErrorPage/ErrorPage";
+import Home from "../Home/Home/Home";
+
+
+export const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <Main></Main> ,
+        errorElement : <ErrorPage> </ErrorPage> ,
+        children : [
+            {
+                path :'/' ,
+                element : <Home></Home>
+            }
+        ]
+
+
+
+        
+    }
+])
