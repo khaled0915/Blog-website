6bc8f7c8673ca45814d8e0ee4790da1727e0017a
khaled0915 <khaledsaifullah50956@gmail.com>
2023-12-02 21:11:43 +0600

    created a simple navbar

--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Navbar from "../../Page/Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+
+            <Navbar ></Navbar>
             <h3> This is Home </h3>
             <h2> Welcome to EchoMinds </h2>
             
--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,93 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+
+    const navBar = <>
+
+    
+    <li>
+                <Link className=" bg-emerald-500 " to='/'>
+                    Home </Link>
+
+            </li>
+            <li>
+                <Link className="bg-green-500" to='/addBlog'>
+                Add Blog
+
+
+                </Link>
+
+            </li>
+            <li>
+
+                
+                 <Link className="bg-pink-500" to='/allBlog'>
+
+
+                 All blogs  
+
+            </Link> </li>
+
+            <li> 
+                
+                <Link className="bg-teal-500" to='/featureBlogs'>
+
+                Featured Blogs 
+                
+                 </Link>
+                 </li>
+            <li> 
+
+                
+                <Link className="bg-teal-500" to='/wishlist'>
+
+                Wishlist 
+                
+                 </Link>
+                 </li>
+
+    
+
+
+
+    
+    
+     </>
     return (
         <div>
-            
+            <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        {navBar}
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+     {navBar}
+    </ul>
+  </div>
+
+
+
+  <div>
+  <div className="navbar-end">
+    <a className="btn"> Login </a>
+  </div>
+
+  <div className="navbar-end">
+    <a className="btn"> Sign_Up </a>
+  </div>
+  </div>
+
+
+</div>
         </div>
     );
 };
