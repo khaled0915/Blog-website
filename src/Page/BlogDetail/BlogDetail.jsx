3142b0c3e84281313f4e37ade7bf438c4b67a3bb
khaled0915 <khaledsaifullah50956@gmail.com>
2023-12-04 17:23:21 +0600

    created the blog detail page

--- src/Page/BlogDetail/BlogDetail.jsx
+++ src/Page/BlogDetail/BlogDetail.jsx
@@ -1,12 +1,16 @@
 
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import useBlog from "../../hooks/useBlog";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { Helmet } from "react-helmet-async";
 
 // import useBlog from "../../hooks/useBlog";
 
 
 const BlogDetail = () => {
+
+    
 
     const item = useLoaderData();
     console.log(item);
@@ -14,9 +18,29 @@ const BlogDetail = () => {
 
     // const {user} = useContext(AuthContext);
 
-    const [blog ] = useBlog();
+    const [blog , refetch ] = useBlog();
 
     console.log(blog);
+
+    const axiosPublic = useAxiosPublic();
+
+    const {id}= useParams();
+    console.log(id);
+
+
+  if (!blog || blog.length === 0) {
+    return <progress className="progress progress-warning w-56" value="100" max="100"></progress>;
+  }
+
+ 
+  const filteredBlog = blog.find(item => item._id === id);
+
+  
+  if (!filteredBlog) {
+    return <p>Camp not found</p>; 
+  }
+
+  console.log(filteredBlog);
 
     
 
@@ -26,9 +50,42 @@ const BlogDetail = () => {
     return (
 
         
-        <div>
+        <div >
+            <Helmet>
+                <title> CampSwift | Blog Details  </title>
+
+            </Helmet>
             <Navbar></Navbar>
-            <h2> This is Blog details {item.length} </h2>
+
+
+        
+            <div className="card mt-10 lg:card-side bg-base-100 shadow-xl">
+  <figure><img className="h-[500px] w-[600px]  " src={filteredBlog.image} alt="Album"/></figure>
+  <div className="card-body bg-red-300">
+    <h2 className="card-title font-bold underline text-2xl"> {filteredBlog.title} </h2>
+    
+    
+    <p className="text-xl font-semibold"> {filteredBlog.shortDescription
+}  </p> 
+
+<p className="text-xl font-semibold text-center mt-10"> {filteredBlog.longDescription
+} </p>
+
+
+    <div className="card-actions justify-end">
+      <button className="btn btn-success  text-center mt-5 mb-5 "> Update </button>
+    </div>
+
+
+
+    <textarea className="textarea textarea-warning" placeholder="comment"></textarea>
+
+
+
+
+  </div>
+</div>
+      
 
           
 
